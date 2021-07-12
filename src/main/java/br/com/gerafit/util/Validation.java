a29416b7dfe9851afc56d8465fec61735afe93d3
Leandro Moreira <leandroangus@gmail.com>
2021-07-12 10:56:00 -0300

    Criando o pacote Util e Classes utilitárias, com mensagens customizadas.

---
+++ src/main/java/br/com/gerafit/util/Validation.java
@@ -0,0 +1,20 @@
+package br.com.gerafit.util;
+
+public class Validation {
+	
+	//Metodo validador
+	public static void assertNotEmpty(Object obj) {
+		if (obj instanceof String) {
+			String s = (String) obj;//Operacao de casting
+			if (StringUtils.isEmpty(s)) {//Chamei o metodo string utils
+				throw new ValidationException("O texto não pode ser nulo!");
+			}
+		}
+		
+		if (obj == null) {
+			throw new ValidationException("Valor não pode ser nulo!");
+		}
+	}
+
+
+}
